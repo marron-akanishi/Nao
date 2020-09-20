496012462bd0f5278e7bb9371d1f2021ec0e8b66
Marron Akanishi <marron.general@gmail.com>
2020-09-20 22:40:22 +0900

    update: スマホ対応

--- src/index.tsx
+++ src/index.tsx
@@ -9,3 +9,15 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+window.onload = () => {
+  // 最初に、ビューポートの高さを取得し、0.01を掛けて1%の値を算出して、vh単位の値を取得
+  let vh = window.innerHeight * 0.01;
+  // カスタム変数--vhの値をドキュメントのルートに設定
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  window.addEventListener("resize", () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+};
