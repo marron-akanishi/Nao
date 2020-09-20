1bf63615b0a84e775376a851528a6d758832fd0f
marron-akanishi <marron-akanishi@users.noreply.github.com>
2020-09-20 07:25:58 +0000

    deploy: 5cb635dea88185b8d84f26dc102b77f1cb406ae4

---
+++ precache-manifest.228780c42fea329e455025efac64cb98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c6f3fc29c93e08936880bfc4606f3d",
+    "url": "/Nao/index.html"
+  },
+  {
+    "revision": "32483218711d6a417952",
+    "url": "/Nao/static/css/main.aef987bc.chunk.css"
+  },
+  {
+    "revision": "4f49669f086763f6b7b9",
+    "url": "/Nao/static/js/2.223e290b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Nao/static/js/2.223e290b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32483218711d6a417952",
+    "url": "/Nao/static/js/main.a5f315e6.chunk.js"
+  },
+  {
+    "revision": "21c00bf95e4547ac176a",
+    "url": "/Nao/static/js/runtime-main.57968c05.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.401dfdeae655d51d3d0ed7133724ecde.js"
+  "/Nao/precache-manifest.228780c42fea329e455025efac64cb98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nao/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
